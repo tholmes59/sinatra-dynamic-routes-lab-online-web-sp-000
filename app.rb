97798c473dc52d8786066732c233c84febec331e
Tom Holmes <tholmes59@gmail.com>
2019-03-23 20:19:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,4 +19,16 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
+  get '/:operation/:number1/:number2' do
+    if params[:operation] == add 
+      "#{params[:number1].to_i} + #{params[:number2].to_i}"
+      elsif params[:operation] == subtract 
+      "#{params[:number1].to_i} - #{params[:number2].to_i}"
+      elsif params[:operation] == multiply
+      "#{params[:number1].to_i} * #{params[:number2].to_i}"
+      elsif params[:operation] == divide
+      "#{params[:number1].to_i} / #{params[:number2].to_i}"
+    end
+  end
+  
 end
