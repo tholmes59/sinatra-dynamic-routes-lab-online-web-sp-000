7672946a0a9775d2bf7b81e6eb43eac5b2fe1957
Tom Holmes <tholmes59@gmail.com>
2019-03-23 20:23:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,13 +21,17 @@ class App < Sinatra::Base
   
   get '/:operation/:number1/:number2' do
     if params[:operation] == add 
-      "#{params[:number1].to_i} + #{params[:number2].to_i}"
+      @number = params[:number1].to_i + params[:number2].to_i
+      "#{@number}"
       elsif params[:operation] == subtract 
-      "#{params[:number1].to_i} - #{params[:number2].to_i}"
+      @number = params[:number1].to_i - params[:number2].to_i
+      "#{@number}"
       elsif params[:operation] == multiply
-      "#{params[:number1].to_i} * #{params[:number2].to_i}"
+      @number = params[:number1].to_i * params[:number2].to_i
+      "#{@number}"
       elsif params[:operation] == divide
-      "#{params[:number1].to_i} / #{params[:number2].to_i}"
+      @number = params[:number1].to_i / params[:number2].to_i
+      "#{@number}"
     end
   end
   
