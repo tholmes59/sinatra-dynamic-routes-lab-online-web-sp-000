f8cc969cb831e5897b7f24eb7695e63a53a21fc6
Tom Holmes <tholmes59@gmail.com>
2019-03-23 20:29:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do
-    
+     
+
+    params[:number].to_i.times do
+      @answer += params[:phrase]
+    end
+
+    "#{@answer}"
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
